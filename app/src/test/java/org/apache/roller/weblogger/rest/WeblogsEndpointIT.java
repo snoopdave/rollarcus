7d3fc058f3c84d66234516116d7a1ce2867530a6
Dave Johnson <dmjohnson@apigee.com>
2014-12-28 17:10:23 -0500

    Move test to same package as test subject.

--- app/src/test/java/org/apache/roller/weblogger/rest/WeblogsEndpointIT.java
+++ app/src/test/java/org/apache/roller/weblogger/rest/WeblogsEndpointIT.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.roller.weblogger.webservices.rest;
+package org.apache.roller.weblogger.rest;
 
 import java.io.File;
 import java.net.URL;
