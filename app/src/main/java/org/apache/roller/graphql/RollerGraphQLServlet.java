5bf9745b201818fbb0388922101bffdb6aa43a1a
David M. Johnson <snoopdave@apache.org>
2020-02-08 08:41:01 -0500

    Add GraphQL dependency and begining of impl.

---
+++ app/src/main/java/org/apache/roller/graphql/RollerGraphQLServlet.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ *
+ */
+package org.apache.roller.graphql;
+
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.servlet.GraphQLConfiguration;
+import graphql.servlet.GraphQLHttpServlet;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class RollerGraphQLServlet extends GraphQLHttpServlet {
+
+    public RollerGraphQLServlet() {
+    }
+
+    @Override
+    protected GraphQLConfiguration getConfiguration() {
+        return GraphQLConfiguration.with(createSchema()).build();
+    }
+
+    private GraphQLSchema createSchema() {
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+            .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+            .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+    }
+}
